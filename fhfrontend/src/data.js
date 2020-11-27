6c249ebe396de6332f3db2eefb520195cf2d9526
Bart Piasek <bartpiasek@icloud.com>
2020-11-27 22:32:35 +0100

    DONE: data.js - hardcoded prod info

--- fhfrontend/src/data.js
+++ fhfrontend/src/data.js
@@ -11,5 +11,38 @@ const data = {
             numReviews:10,
             descriptions:'Foodhunterki ebook nr1'
         },
+        {
+            _id:'2',
+            name:'Foodhunterki ebook 2',
+            category:'Ebook',
+            image:'/images/prod-2.jpg',
+            price:150,
+            brand:'Foodhunterki',
+            rating: 5,
+            numReviews:20,
+            descriptions:'Foodhunterki ebook nr2'
+        },
+        {
+            _id:'3',
+            name:'Foodhunterki ebook 3',
+            category:'Ebook',
+            image:'/images/prod-3.jpg',
+            price:170,
+            brand:'Foodhunterki',
+            rating: 4,
+            numReviews:50,
+            descriptions:'Foodhunterki ebook nr3'
+        },
+        {
+            _id:'4',
+            name:'Szkolenie Social Media',
+            category:'Szkolenia',
+            image:'/images/prod-3.jpg',
+            price:1000,
+            brand:'Foodhunterki',
+            rating: 4,
+            numReviews:100,
+            descriptions:'Foodhunterki szkolenie SM'
+        },
     ],
 };
