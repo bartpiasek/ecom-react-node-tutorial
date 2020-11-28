51782dc7118f61f5dfde7db973d4a9d1ee9783e9
Bart Piasek <bartpiasek@icloud.com>
2020-11-28 10:47:42 +0100

    BUG FIX: array callback

--- fhfrontend/src/App.js
+++ fhfrontend/src/App.js
@@ -6,20 +6,28 @@ function App() {
     <div className="grid-container">
         <header className="row">
             <div>
-                <a className="brand" href="index.html">Foodhunterki</a>
+                <a className="brand" href="/">Foodhunterki</a>
             </div>
             <div>
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
             </div>
         </header>
         <main>
             <div className="row center">
-              {data.products.map((product) => {
+              {data.products.map((product) => (
                   <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}><img className="medium" src={product.image} alt={product.name} /></a>
+                    <a href={`/product/${product._id}`}>
+                      <img 
+                      className="medium" 
+                      src={product.image} 
+                      alt={product.name} 
+                      />
+                    </a>
                     <div className="card-body">
-                        <a href={`/product/${product._id}`}><h3>{product.name}</h3></a>
+                        <a href={`/product/${product._id}`}>
+                          <h3>{product.name}</h3>
+                        </a>
                         <div className="rating">
                             <span><i className="fa fa-star"></i></span>
                             <span><i className="fa fa-star"></i></span>
@@ -27,12 +35,11 @@ function App() {
                             <span><i className="fa fa-star"></i></span>
                             <span><i className="fa fa-star"></i></span>
                         </div>
-                        <div className="price">${product.price}</div>
+                        <div className="price">PLN {product.price}</div>
                     </div>
                   </div>
-                })
-              }
-                
+              ))
+              }  
             </div>
         </main>
         <footer className="row center">
