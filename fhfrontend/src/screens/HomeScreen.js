b138b926369e3c1f060e7cda00c9d08d2015acaf
Bart Piasek <bartpiasek@icloud.com>
2020-11-29 11:25:37 +0100

    FIX: list of product in productdetail page

--- fhfrontend/src/screens/HomeScreen.js
+++ fhfrontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import data from '../data';
 
 export default function HomeScreen() {
     return (
+        
         <div className="row center">
               {data.products.map((product) => (
                 <Product key={product._id} product={product}></Product> 
--- fhfrontend/src/screens/ProductScreen.js
+++ fhfrontend/src/screens/ProductScreen.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import Rating from '../components/Rating';
+import data from '../data';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const product = data.products.find(x => x._id === props.match.params.id);
+    if (!product) {
+        return <div>Product not found</div>;
+    }
     return (
         <div>
-            Product Screen
+            <div className="row">
+                <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name}></img>
+                </div>
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating 
+                                rating={product.rating}
+                                numReviews={product.numReviews}
+                            ></Rating>
+                        </li>
+                        <li>
+                            Price: PLN {product.price}
+                        </li>
+                        <li>
+                            Description: 
+                            <p>{product.descriptions}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-1">
+                    <div className="card card-body">
+                        <ul>
+                            <li>
+                                <div className="row">
+                                    <div>Price</div>
+                                    <div className="price">
+                                        PLN {product.price}
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="row">
+                                    <div>Status</div>
+                                    <div>
+                                        {product.countInStock > 0 
+                                        ? (<span className="success">In stock</span>)
+                                        : (<span className="error">Out of stock</span>)
+                                    }
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Add to cart</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr></hr>
         </div>
     );
 }
