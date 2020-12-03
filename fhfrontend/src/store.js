e36c1052ca8883c152309256466adb2e0a05d6ce
Bart Piasek <bartpiasek@icloud.com>
2020-12-03 20:09:06 +0100

    REDUX: install thunk, store.js composeEnhancer

--- fhfrontend/src/store.js
+++ fhfrontend/src/store.js
@@ -1,11 +1,17 @@
 import data from './data';
-import {createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
 // REDUX - initial state
 const initialState = {};
 const reducer = (state, action) => {
     return { products: data.products };
 };
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, initialState);
+const store = createStore(
+    reducer, 
+    initialState, 
+    composeEnhancer(applyMiddleware(thunk))
+    );
 
 export default store;
