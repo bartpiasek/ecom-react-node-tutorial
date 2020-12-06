cc9bc78f2be30ce6cbcc88d6d446897b99a34264
Bart Piasek <bartpiasek@icloud.com>
2020-12-06 19:36:11 +0100

    buttons in cart, design

--- fhfrontend/src/screens/cartScreen.js
+++ fhfrontend/src/screens/cartScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { addToCart } from '../actions/cartAction';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import MessageBox from '../components/MessageBox';
+import { Link } from 'react-router-dom';
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
@@ -8,6 +10,8 @@ export default function CartScreen(props) {
     ? Number(props.location.search.split('=')[1])
     : 1;
 
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
     const dispatch = useDispatch();
     useEffect(() => {
         if (productId) {
@@ -15,10 +19,66 @@ export default function CartScreen(props) {
         }
     }, [dispatch, productId, qty]);
 
+    const removeFromCartHandler = (id) => {
+        // delete action
+        
+    };
+
     return (
-        <div>
-           <h1>Koszyk</h1>
-           <p>Dodaj to koszyka : ProductID: {productId} Ilość: {qty}</p>
+        <div className="row top">
+            <div className="col-2">
+                <h1>Koszyk</h1>
+                {cartItems.length === 0
+                ? <MessageBox>Koszyk jest pusty 
+                    <Link to="/">Strona główna</Link>
+                  </MessageBox>
+                : <ul>
+                    {cartItems.map((item) => (
+                        <li key={item.product}>
+                            <div className="row">
+                                <div>
+                                    <img 
+                                    src={item.image} 
+                                    alt={item.name}
+                                    className="small"
+                                ></img>
+                                </div>
+                                <div className="min-30">
+                                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                </div>
+                                <div>
+                                    <select 
+                                        value={item.qty} 
+                                        onChange={e => 
+                                            dispatch(
+                                                addToCart(item.product), 
+                                                Number(e.target.value)
+                                            )
+                                        }>
+                                        {[...Array(item.countInStock).keys()].map(
+                                            (x) => (
+                                            <option key={x + 1} value={x + 1}>
+                                                {x + 1}
+                                            </option>
+                                            )
+                                        )}
+                                        </select>
+                                </div>
+                                <div>
+                                    PLN {item.price}
+                                </div>
+                                <div>
+                                    <button 
+                                        type="button" 
+                                        onClick={() => removeFromCartHandler(item.product)}
+                                    >Usuń</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                }
+            </div>
         </div>
     );
 }
