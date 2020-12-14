29d853b57accb2a069762dfceed3345ea8047552
Bart Piasek <bartpiasek@icloud.com>
2020-12-14 22:51:08 +0100

    2/2 order api, order router, middleware in utils, auth

---
+++ fhbackend/routers/orderRouter.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+import { isAuth } from '../utils.js';
+
+const orderRouter = express.Router();
+
+orderRouter.post(
+    '/',
+    isAuth, // <-- utils.js 
+    expressAsyncHandler(async(req, res) => {
+    if (req.body.orderItems.length === 0) {
+        res.status(400).send({ message: 'cart is empty'});
+    } else {
+        const order = new Order({
+            orderItems: req.body.orderItems,
+            shippingAddress: req.body.shippingAddress,
+            paymentMethod: req.body.paymentMethod,
+            itemsPrice: req.body.itemsPrice,
+            shippingPrice: req.body.shippingPrice,
+            taxPrice: req.body.taxPrice,
+            totalPrice: req.body.totalPrice,
+            // to get info of user, we define middleware - utils.js
+            user: req.user._id,
+        });
+        const createdOrder = await Order.save();
+        res
+            .status(201)
+            .send({ message: 'new order created', order: createdOrder });
+    }
+})
+);
+export default orderRouter;
--- fhbackend/utils.js
+++ fhbackend/utils.js
@@ -14,3 +14,22 @@ export const generateToken = (user) => {
     }
     );
 };
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(authorization) {
+        const token = authorization.slice(7, authorization.length);
+        jwt.verify(token, process.env.JWT_SECRET || 'something', 
+        //callback func
+        (err, decode) => {
+            if(err) {
+                req.status(401).send({ message: 'invalid token' });
+            } else {
+                req.user = decode;
+                next();
+            }
+        });
+    } else {
+        req.status(401).send({ message: 'no token' });
+    }
+};
