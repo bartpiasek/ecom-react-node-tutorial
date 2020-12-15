3161c0703b76635047717027172726442f316134
Bart Piasek <bartpiasek@icloud.com>
2020-12-15 19:58:55 +0100

    backend api for orderhistory

--- fhbackend/routers/orderRouter.js
+++ fhbackend/routers/orderRouter.js
@@ -4,6 +4,15 @@ import Order from '../models/orderModel.js';
 import { isAuth } from '../utils.js';
 
 const orderRouter = express.Router();
+// only authenticated users have access to orderhistory
+orderRouter.get(
+    '/mine', 
+    isAuth, 
+    expressAsyncHandler(async(req, res) => {
+        const orders = await Order.find({ user: req.user._id });
+        res.send(orders);
+    })
+);
 
 orderRouter.post(
     '/',
