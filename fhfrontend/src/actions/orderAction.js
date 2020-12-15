cce95b5d9dc9f83bfec0d7cf00880d21c5f58d93
Bart Piasek <bartpiasek@icloud.com>
2020-12-15 21:35:42 +0100

    refactoring

--- fhfrontend/src/actions/orderAction.js
+++ fhfrontend/src/actions/orderAction.js
@@ -1,6 +1,16 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_MINE_LIST_FAIL, ORDER_MINE_LIST_REQUEST, ORDER_MINE_LIST_SUCCESS } from "../constants/orderConstant";
 import Axios from 'axios';
 import { CART_EMPTY } from "../constants/cartConstant";
+import { 
+    ORDER_CREATE_FAIL, 
+    ORDER_CREATE_REQUEST, 
+    ORDER_CREATE_SUCCESS, 
+    ORDER_DETAILS_FAIL, 
+    ORDER_DETAILS_REQUEST, 
+    ORDER_DETAILS_SUCCESS, 
+    ORDER_MINE_LIST_FAIL, 
+    ORDER_MINE_LIST_REQUEST, 
+    ORDER_MINE_LIST_SUCCESS 
+} from "../constants/orderConstant";
 
 export const createOrder = (order) => async (dispatch, getState) => {
     dispatch({ type: ORDER_CREATE_REQUEST, payload: order });
@@ -13,9 +23,9 @@ export const createOrder = (order) => async (dispatch, getState) => {
             },
         });
         dispatch({ type: ORDER_CREATE_SUCCESS, payload: data.order});
-        dispatch({ type: CART_EMPTY});
+        dispatch({ type: CART_EMPTY });
         localStorage.removeItem('cartItems');
-    } catch(error) {
+    } catch (error) {
         dispatch({ 
             type: ORDER_CREATE_FAIL,
             payload: error.response && error.response.data.message
@@ -27,13 +37,13 @@ export const createOrder = (order) => async (dispatch, getState) => {
 
 export const detailsOrder = (orderId) => (dispatch, getState) => {
     dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
-    const {userSignin: {userInfo}} = getState();
+    const { userSignin: { userInfo }} = getState();
     try {
         const { data } = await Axios.get(`/api/orders/${orderId}`, {
            headers: { Authorization: `Bearer ${userInfo.token}`}, 
         });
         dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data });
-    } catch(error) {
+    } catch (error) {
         const message = error.response && error.response.data.message
         ? error.response.data.message
         : error.message;
@@ -43,18 +53,18 @@ export const detailsOrder = (orderId) => (dispatch, getState) => {
 
 export const listOrderMine = () => async (dispatch, getState) => {
     dispatch({ type: ORDER_MINE_LIST_REQUEST });
-    const { userSignin:{userInfo}} = getState();
+    const { userSignin: { userInfo }} = getState();
     try {
-        cont { data } = await Axios.get('/api/orders/mine', {
+        const { data } = await Axios.get('/api/orders/mine', {
             headers: {
                 Authorization: `Bearer ${userInfo.token}`,
             },
         });
-        dispatch({ type:ORDER_MINE_LIST_SUCCESS, payload: data });
+        dispatch({ type: ORDER_MINE_LIST_SUCCESS, payload: data });
     } catch(error) {
         const message = error.response && error.response.data.message
             ? error.response.data.message
             : error.message;
         dispatch({ type: ORDER_MINE_LIST_FAIL, payload: message });
     }
-}
+};
